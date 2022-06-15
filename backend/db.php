508b2ac36e4d695a8e91af1baba2e5206b52dec8
bhgh001 <83581211+bhgh001@users.noreply.github.com>
2022-06-15 20:56:03 +0530

    jwt token eith hostinger changes

--- backend/db.php
+++ backend/db.php
@@ -1,6 +1,6 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '');
-    $database = mysqli_select_db($conn, 'sayhey-app');
+    $conn = mysqli_connect('localhost', 'u819537982_root', 'Rootpass123');
+    $database = mysqli_select_db($conn, 'u819537982_sayheydb');
 
     $encodedData = file_get_contents('php://input');  // take data from react native fetch API
     $decodedData = json_decode($encodedData, true);
