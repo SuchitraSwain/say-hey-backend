54c2368213b8a3ffc8e1040963b8eb345c453f47
bhgh001 <83581211+bhgh001@users.noreply.github.com>
2022-06-18 15:58:59 +0530

    review api

---
+++ backend/review_ret.php
@@ -0,0 +1,30 @@
+<?php
+    include('db.php');
+
+    $session_id = $_GET['sesssion_id'];
+    $sessionSearch = "SELECT  *  
+from review 
+JOIN session ON review.session_id = session.session_id
+JOIN doctor ON session.doc_id=doctor.userid
+WHERE review.session_id = '$session_id'";
+        $searchAction = mysqli_query($conn, $sessionSearch);
+        $totalSessRev = mysqli_num_rows($searchAction);
+        if($totalSessRev!=0){
+            $row = mysqli_fetch_assoc($searchAction);
+            $review =$row['review'];
+            $rating = $row['rating'];
+            $doc_name =$row['name'];
+            $Message ="Sucess";
+            $response[] = array("Message" => $Message,
+                        "review"=> $review,
+                        "rating"=> $rating,
+                        "Dr name" => $doc_name
+                        );
+        }
+        else{
+            $Message ="So such session";
+            $response[] = array("Message" => $Message);
+        }
+        echo json_encode($response);
+
+?>
